6832ce1e757b26ee1a7bc9cb544f548480c160c9
RadialApps <radialapps@gmail.com>
2017-10-01 15:33:13 +0530

    Use csv module instead of pandas

--- radsh.py
+++ radsh.py
@@ -4,10 +4,19 @@
 # Copyright 2017 (C) RadialApps <radialapps@gmail.com>
 #
 
-import pandas as pd
 import re
 import sys
 import os
+import csv
+
+# Regex to be used
+regexsquare = r"\[(.*?)\]"
+regexcurl = r"{{([^}]+)}}"
+regexcaret = r"\^\^([^}]+)\^\^"
+regexdollar = r"\$\$(.*?)\$\$"
+regexnotdollar = r"\$!(.*?)!\$"
+
+current_row = {}
 
 # Check arguments
 if len(sys.argv) != 4:
@@ -19,17 +28,17 @@ for i in range(1,3):
         print("File not found --", sys.argv[i])
         quit()
 
-# Read our data into a pandas dataframe
+# Read the data
 print('Reading data')
-df = pd.read_csv(sys.argv[1])
+with open(sys.argv[1], 'r') as csvfile:
+    data_rows = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+    data_columns = data_rows[0]
+    del data_rows[0]
 
 # Read the template file
 print('Reading template')
-with open(sys.argv[2]) as f:
-    template = f.read()
-
-# Just to be safe
-rowno = 0
+with open(sys.argv[2]) as template_file:
+    template = template_file.read()
 
 # Replace any unsafe characters for http links
 # TODO: expand this to replace other special characters
@@ -48,16 +57,16 @@ def preprocess(match):
     # Look for conditions
     for col in re.findall(regexcaret, com):
         # Check if such a column exists
-        if not col in df.columns:
+        if not col in data_columns:
             continue
 
-        if df[col][rowno] == 1:
+        if current_row[col] == "1":
             # Answer if true
             if len(re.findall(regexdollar, com)) > 0:
                 print(col, ' == true -- ', re.findall(regexdollar, com)[0])
                 return re.findall(regexdollar, com)[0]
 
-        elif df[col][rowno] == 0:
+        elif current_row[col] == "0":
             # Answer if false
             if len(re.findall(regexnotdollar, com)) > 0:
                 print(col, ' == false -- ', re.findall(regexnotdollar, com)[0])
@@ -74,55 +83,51 @@ def compile(match):
     col = match.strip("{}")
 
     # Look for things to do
-    if col in df.columns:
+    if col in data_columns:
         # If a file is to be substituted
         # Throws an error if the file is not found
-        if 'file=' in df[col][rowno]:
-            print ('Inserting file', df[col][rowno][5:])
-            with open(df[col][rowno][5:]) as fl:
+        if 'file=' in current_row[col]:
+            print ('Inserting file', current_row[col][5:])
+            with open(current_row[col][5:]) as fl:
                 temp = fl.read()
 
             # Recursively process the file
             return re.sub(regexcurl,compile, re.sub(regexsquare, preprocess, temp))
 
         # Return the value of the requested cell
-        print (col, ' -- ', df[col][rowno])
+        print (col, ' -- ', current_row[col])
 
         # Look for http links and replace special characters
         # TODO: If necessary, replace special characters everywhere
-        if df[col][rowno][:4] == 'http':
+        if current_row[col][:4] == 'http':
             print('Detected http link')
-            return replace_special(df[col][rowno])
+            return replace_special(current_row[col])
 
-        return df[col][rowno]
+        return current_row[col]
     else:
         # No such column
         return match
 
-# Regex to be used
-regexsquare = r"\[(.*?)\]"
-regexcurl = r"{{([^}]+)}}"
-regexcaret = r"\^\^([^}]+)\^\^"
-regexdollar = r"\$\$(.*?)\$\$"
-regexnotdollar = r"\$!(.*?)!\$"
-
 # Main loop
-for index,row in df.iterrows():
-    # Update index for other functions
-    rowno = index
+for index, raw_row in enumerate(data_rows):
 
-    if str(row['filename']) == 'nan':
+    # Get the current row
+    for i, col in enumerate(data_columns):
+        current_row[col] = raw_row[i]
+
+    # Check if something is bad
+    if str(current_row['filename']) == 'nan':
         continue;
 
     # Get the file to save to
-    filename = row['filename'] + '.' + sys.argv[3]
+    filename = current_row['filename'] + '.' + sys.argv[3]
     print(' ------------ Now working on ', filename, '------------')
 
     # Start working
     final = re.sub(regexcurl, compile, re.sub(regexsquare, preprocess, template))
 
     # Write out everything when done
-    with open(filename, 'w') as f:
-        f.write(final)
+    with open(filename, 'w') as output_file:
+        output_file.write(final)
 
 print('Everything done')
